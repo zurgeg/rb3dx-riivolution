dbe927884b03d68827f22a2faac9b8be4bd907c2
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-10-11 14:30:01 -0400

    Add auto-download and stuff

--- build-rb3dx.py
+++ build-rb3dx.py
@@ -1,8 +1,9 @@
 from os.path import isdir, exists, join
-from os import cwd
-from click import Choice, prompt, echo
+from os import chdir, curdir
+from click import Choice, prompt, confirm, echo
 from alive_progress import alive_bar
 from subprocess import Popen, DEVNULL, PIPE
+from shutil import move
 
 menu_choices = Choice(["Make ISO", "Make Riivolution"])
 
@@ -15,6 +16,53 @@ if what_to_do == "Make ISO":
 else:
     where_is_wit = prompt("WIT Location")
     where_is_rb3 = prompt("RB3 Location")
+    while True:
+        if not isdir(join(curdir, "temp", "rock-band-3-deluxe")):
+            has_rb3dx = confirm("Do you have RB3DX downloaded?")
+            if has_rb3dx:
+                where_is_rb3dx = prompt("RB3DX Location")
+            else:
+                where_is_rb3dx = join(curdir, "temp", "rock-band-3-deluxe")
+                echo("Okay. I'll download RB3DX now...")
+                keep_rb3dx = confirm("By the way... should I keep RB3DX afterwards? If you run this utility afterwards, I'll update and use the previously downloaded RB3DX.")
+                echo("Alright. I'll check for Git now...")
+                try:
+                    Popen(["git"], stdout=DEVNULL, stderr=DEVNULL).wait()
+                except:
+                    echo("Sorry! I couldn't find Git! Please install it and try again.")
+                    exit(1)
+                echo("Okay. We are ready to clone the repository. Please wait...")
+                statuscode = Popen(["git", "clone", "https://github.com/hmxmilohax/rock-band-3-deluxe", "./temp/rock-band-3-deluxe"], stdout=DEVNULL, stderr=DEVNULL).wait()
+                if statuscode:
+                    echo("Uh oh! We had a problem running Git. Try again later.")
+                    echo(f"(In case you wanted to know, the status code was {statuscode})")
+                    exit()
+                else:
+                    echo("Alright! I've downloaded RB3DX!")
+        else:
+            while True:
+                echo("Oh! It looks like you have RB3DX downloaded already! Awesome! I'll update that real quick...")
+                old_dir = curdir
+                chdir(join(curdir, "temp", "rock-band-3-delue"))
+                statuscode = Popen(["git", "pull"], stdout=DEVNULL, stderr=DEVNULL).wait()
+                if statuscode:
+                    use_other_rb3dx = prompt("Sorry! I couldn't update RB3DX! Do you want to use a different copy of RB3DX?")
+                    if use_other_rb3dx:
+                        # We'll move the cloned RB3DX temporarily
+                        chdir(old_dir)
+                        # We'll move this back during cleanup.
+                        move(join(curdir, "temp", "rock-band-3-delue"), join(curdir, "temp", "rb3dx-update-fail"))
+                        do_break = False
+                        break
+                else:
+                    where_is_rb3dx = join(curdir, "temp", "rock-band-3-deluxe")
+                    do_break = True
+                    break
+            if do_break:
+                break
+
+
+
     with alive_bar(title="Making Riivolution Patch", total=0, enrich_print=False) as bar:
         bar.title(f"Checking if WIT (at {where_is_wit}) works...")
         # 21 lines of janky idiot protection.
@@ -54,7 +102,7 @@ else:
                 bar()
                 bar.title(f"Extracting Rock Band 3...")
                 bar()
-                Popen([where_is_wit, "extract", where_is_rb3, join(cwd(), "temp", "extracted_rb3")]).wait()
+                Popen([where_is_wit, "extract", where_is_rb3, join(curdir, "temp", "extracted_rb3")]).wait()
                 bar()
                 
 
