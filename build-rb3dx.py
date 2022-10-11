323e549662ae3db6f9541ba95ea1a5405ca0cb35
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-10-11 14:57:27 -0400

    h

--- build-rb3dx.py
+++ build-rb3dx.py
@@ -1,12 +1,20 @@
-from os.path import isdir, exists, join
+from os.path import isdir, exists, join, basename
 from os import chdir, curdir
 from click import Choice, prompt, confirm, echo
 from alive_progress import alive_bar
 from subprocess import Popen, DEVNULL, PIPE
-from shutil import move
+from shutil import copy, move
+from glob import glob
+from threading import Thread
 
 menu_choices = Choice(["Make ISO", "Make Riivolution"])
 
+def call_bar_at_a_very_fast_rate(bar):
+    try:
+        while True:
+            bar()
+    except:
+        pass
 
 print("---Welcome to zurgeg's RB3DX build tools!---")
 what_to_do = prompt("What do you want to do?", prompt_suffix=": ", show_choices=True, type=menu_choices)
@@ -21,6 +29,7 @@ else:
             has_rb3dx = confirm("Do you have RB3DX downloaded?")
             if has_rb3dx:
                 where_is_rb3dx = prompt("RB3DX Location")
+                break
             else:
                 where_is_rb3dx = join(curdir, "temp", "rock-band-3-deluxe")
                 echo("Okay. I'll download RB3DX now...")
@@ -44,6 +53,7 @@ else:
                 echo("Oh! It looks like you have RB3DX downloaded already! Awesome! I'll update that real quick...")
                 old_dir = curdir
                 chdir(join(curdir, "temp", "rock-band-3-delue"))
+                
                 statuscode = Popen(["git", "pull"], stdout=DEVNULL, stderr=DEVNULL).wait()
                 if statuscode:
                     use_other_rb3dx = prompt("Sorry! I couldn't update RB3DX! Do you want to use a different copy of RB3DX?")
@@ -55,6 +65,7 @@ else:
                         do_break = False
                         break
                 else:
+                    Popen(["git", "checkout", "wii"], stdout=DEVNULL, stderr=DEVNULL).wait()
                     where_is_rb3dx = join(curdir, "temp", "rock-band-3-deluxe")
                     do_break = True
                     break
@@ -64,6 +75,8 @@ else:
 
 
     with alive_bar(title="Making Riivolution Patch", total=0, enrich_print=False) as bar:
+        t = Thread(target=call_bar_at_a_very_fast_rate, name="fast", args=[bar])
+        t.start()
         bar.title(f"Checking if WIT (at {where_is_wit}) works...")
         # 21 lines of janky idiot protection.
         while True:
@@ -90,6 +103,8 @@ else:
         bar.title(f"Checking if game (at {where_is_rb3}) is a valid RB3 Wii game...")
         bar()
         # We won't even try with the idiot protection here.
+        if where_is_rb3.startswith("\""):
+            where_is_rb3 = where_is_rb3[1:-1]
         if exists(where_is_rb3) and not isdir(where_is_rb3):
             bar()
             id6_process = Popen([where_is_wit, "id6", where_is_rb3], stdout=PIPE, stderr=PIPE)
@@ -98,13 +113,29 @@ else:
             bar()
             id6_process.wait()
             bar()
-            if id6_stdout.startswith("SZE"):
+            if id6_stdout.startswith(b"SZB"):
                 bar()
                 bar.title(f"Extracting Rock Band 3...")
                 bar()
-                Popen([where_is_wit, "extract", where_is_rb3, join(curdir, "temp", "extracted_rb3")]).wait()
+                if not isdir(join(curdir, "temp", "extracted_rb3")):
+                    Popen([where_is_wit, "extract", where_is_rb3, join(curdir, "temp", "extracted_rb3")], stdout=PIPE, stderr=PIPE).wait()
                 bar()
-                
+                arks = glob(join(curdir, "temp", "extracted_rb3", "files", "gen") + "/*.ark")
+                for ark in arks:
+                    bar()
+                    bar.title(f"Copying ARK {ark}")
+                    bar()
+                    dest = join(where_is_rb3dx, "_build", "wii", basename(ark))
+                    bar()
+                    if not isdir(dest):
+                        raise Exception("This **probably** should never happen. But just in case, you forgot to checkout the wii branch")
+                    bar()
+                    copy(ark, dest)
+            else:
+                echo(id6_stdout)
+                raise Exception("Oh... You thought you could get around ME? AND MY IDIOT PROTECTION? NOT TODAY!")
+        else:
+            raise Exception("I am tired of having to write idiot protection... that's not a file")
 
             
 
